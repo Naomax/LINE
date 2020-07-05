4763890f36407026bb8ec8a06b02247420efffd7
Naomax <naotoz143@gmail.com>
2020-07-05 20:04:35 +0900

    fifth

--- 1.py
+++ 1.py
@@ -1,33 +1,53 @@
-from flas import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import(MessageEvent, TextMessage, TextSend Message,)
-
+from flask import Flask, request, abort
 import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ{"Z0YxYGEsYeu1Pzt40kvWY3rLDnxIg/JMneyX3IheisMKm1PVLLYHFhVAwn7svL73AzOF99SZgtmhoZ2QRvr9ou9tgTwZ3MQf0Cj5lBYjDhJPQholfik0z53eCYjrdcMiJfPh316PM3Onzdh0yFOxawdB04t89/1O/w1cDnyilFU="}
-YOUR_CHANNEL_SECRET = os.envision{"Z0YxYGEsYeu1Pzt40kvWY3rLDnxIg/JMneyX3IheisMKm1PVLLYHFhVAwn7svL73AzOF99SZgtmhoZ2QRvr9ou9tgTwZ3MQf0Cj5lBYjDhJPQholfik0z53eCYjrdcMiJfPh316PM3Onzdh0yFOxawdB04t89/1O/w1cDnyilFU="}
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api=LineBotApi{YOUR_CHANNEL_ACCESS_TOKEN}
-handler=WebhookHandler{YOUR_CHANNEL_SECRET}
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/callback",methods=['POST'])
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature=request.headers{'X-Line-Signature'}
-    body=request.get_data(as_text=True)
-    app.logger.info("Request body: "+ body)
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    
-    return 'OK'
-@handler.add(MessageEvent,message=TextMessage)
-def  handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-if __name__=="__main__":
-    port=int(os.getenv("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
