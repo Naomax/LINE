8ec4448f7da3fd18a6a3c7ffa2c60b1678b220fd
naomax <naotoz143@gmail.com>
2020-07-26 13:59:40 +0900

    i

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 import os
-
+import json
+import psycopg2
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -14,8 +15,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = 'Z0YxYGEsYeu1Pzt40kvWY3rLDnxIg/JMneyX3IheisMKm1PVLLYHFhVAwn7svL73AzOF99SZgtmhoZ2QRvr9ou9tgTwZ3MQf0Cj5lBYjDhJPQholfik0z53eCYjrdcMiJfPh316PM3Onzdh0yFOxawdB04t89/1O/w1cDnyilFU='
+YOUR_CHANNEL_SECRET = 'ac9df2376fe7721c2dde07a52d54c1af'
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -23,7 +24,22 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 @app.route("/")
 def hello_world():
     return "hello world!"
+def get_connection():
+    dsn="host=ec2-35-172-73-125.compute-1.amazonaws.com port=5432 dbname=d1imih4v4olpv3 user=hgxrgiljdbmgur password=4bc62cfb1c5a966928e6263b84d180a10ba951c913ade0f3966288b6e602a4d5"
+    return psycopg2.connect(dsn)
+def get_response_message(mes_from):
+    with get_connection() as conn:
+        print('database connected successfullt')
+        with conn.cursor() as cur:
+            try:
+                sql="INSERT INTO test(content) VALUES(%s);"
+                cur.execute(sql,(mes_from,))
+                return "succeed"
+            except:
+                return "exception"
+    return "???"
 
+            
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -40,12 +56,11 @@ def callback():
         abort(400)
 
     return 'OK'
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        text=get_response_message(event.message.text))
 
 if __name__ == "__main__":
 #    app.run()
